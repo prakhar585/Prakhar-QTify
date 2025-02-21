1a223f7defa7a4097ed31fc2f007f78a8b548635
unknown <prakharchaturvedi9860@gmail.com>
2025-02-21 17:32:23 +0530

    Music card component created

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
+import MusicCard from './components/Card/MusicCard';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Hero/>
+      <MusicCard/>
     </div>
   );
 }
---
+++ qtify/src/components/Card/MusicCard.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { Card, CardMedia, CardContent, Chip, Box } from "@mui/material";
+import demoImg from './../../assets/test-card.png';
+import './MusicCard.css';
+
+const MusicCard = () => {
+  return (
+    <div>
+      <Card className="Card">
+        <Box className="inside-container-card">
+        <CardMedia
+          sx={{ height: 170, width:159,  }}
+          image={demoImg}
+          title="demo test"
+        />
+        <Box className="content-container">
+        <Chip label="100 follows" className="chip" />
+        </Box>
+        </Box>
+        <Box className="footer-text">
+          New english Songs
+        </Box>
+      </Card>
+    </div>
+  );
+};
+
+export default MusicCard;
+
+/*
+    big container
+    width: 159px;
+    height:232px;
+    gap:6px;
+
+    small container:
+    width:159px;
+    height:205px;
+    top right and left radius: 10px;
+    background color: white;
+    
+    image container inside small container:
+    width:159px;
+    height:170px;
+    top right and left radius: 10px;
+
+    chip below the image
+    width: 71;
+    height: 23;
+    top: 176px;
+    left: 8px;
+    gap: 1px;
+    border-radius: 10px;
+    padding-top: 4px;
+    padding-right: 8px;
+    padding-bottom: 4px;
+    padding-left: 8px;
+
+    last text:
+    width: 130px;
+    height: 21px;
+
+    typography:
+    font-family: Poppins;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 21px;
+    letter-spacing: 0px;
+
+
+
+
+*/
